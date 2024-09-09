3620fcfc54532ea825b6a3bb0ff9d3743b14d4e4
Vu Duy Le <vuduyle004@gmail.com>
2024-09-09 07:49:18 +0700

    JPA

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with Vu Duy Le!";
+        return "Hello World with Vu Duy LeLe!";
     }
 
+    @GetMapping("/user")
+    public String userPage() {
+        return "User";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Admin";
+    }
 }
